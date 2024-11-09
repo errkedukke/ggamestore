8cdc090f75118909eb257503dcde5a1a8041250f
guga_rukhadze <guga_rukhadze@epam.com>
2024-11-10 03:00:49 +0400

    added mappy profiles

--- Gamestore.Application/MappingProfiles/GamestoreProfile.cs
+++ Gamestore.Application/MappingProfiles/GamestoreProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Gamestore.Application.Features.Categories.Queries;
+using Gamestore.Application.Features.Comments.Queries;
 using Gamestore.Application.Features.Game.Queries;
+using Gamestore.Application.Features.Genres.Queries;
 using Gamestore.Domain;
 
 namespace Gamestore.Application.MappingProfiles;
@@ -10,6 +12,8 @@ public class GamestoreProfile : Profile
     public GamestoreProfile()
     {
         CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Comment, CommentDto>().ReverseMap();
         CreateMap<Game, GameDto>().ReverseMap();
+        CreateMap<Genre, GenreDto>().ReverseMap();
     }
 }
