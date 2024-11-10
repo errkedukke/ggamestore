caf05ece3a3c8a9cfd4a778a053d1532433cf68b
guga_rukhadze <guga_rukhadze@epam.com>
2024-11-11 00:19:50 +0400

    mc

--- Gamestore.Application/Exceptions/BadRequestException.cs
+++ Gamestore.Application/Exceptions/BadRequestException.cs
@@ -1,5 +1,6 @@
 ﻿namespace Gamestore.Application.Exceptions;
 
-public class BadRequestException
+public class BadRequestException : Exception
 {
+    public BadRequestException(string message) : base(message) { }
 }
