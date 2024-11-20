6801f397e592ba3955a7a56dde8894b7fb7df878
Guchikata <38571128+errkedukke@users.noreply.github.com>
2024-11-21 03:14:18 +0400

    added API

---
+++ Gamestore.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gamestore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(10);
+            return Ok("ok");
+        }
+    }
+}
